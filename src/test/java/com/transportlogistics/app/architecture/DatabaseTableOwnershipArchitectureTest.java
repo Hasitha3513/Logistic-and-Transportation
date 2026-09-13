9e5510498cb7fc15c955afc8178fd67ee209fab0
hasitha843@gmail.com <hasitha843@gmail.com>
2026-09-13 16:06:22 +0530

    Register tracking position history table ownership

--- src/test/java/com/transportlogistics/app/architecture/DatabaseTableOwnershipArchitectureTest.java
+++ src/test/java/com/transportlogistics/app/architecture/DatabaseTableOwnershipArchitectureTest.java
@@ -173,7 +173,7 @@ class DatabaseTableOwnershipArchitectureTest {
                 "transport_billing_cost_centre", "transport_billing_tax_fact", "transport_billing_source_claim",
                 "transport_billing_history");
         own(ownership, "tracking", "tracking_device", "tracking_vehicle_device_assignment", "tracking_position",
-                "tracking_vehicle_latest", "tracking_ingest_nonce", "tracking_audit_event",
+                "tracking_position_history", "tracking_vehicle_latest", "tracking_ingest_nonce", "tracking_audit_event",
                 "tracking_provider_binding", "tracking_provider_ingest_nonce", "tracking_retention_policy",
                 "tracking_device_provider_binding", "tracking_geofence",
                 "tracking_vehicle_geofence_state", "tracking_geofence_transition",
